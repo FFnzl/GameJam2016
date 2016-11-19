55510ad309f8f0562b6ece81f066e1920c9dd721
Dominik <fahldomi@hotmail.de>
2016-11-19 19:04:00 +0100

    Implements Furniture Logic and Timer

---
+++ Assets/dom/prefabs/NewBehaviourScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+    private Rigidbody2D rb;
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+        rb.AddForce(Vector2.left * 100);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+    }
+}
---
+++ Assets/dom/prefabs/ScoreTextBehaviour.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using DG.Tweening;
+
+public class ScoreTextBehaviour : MonoBehaviour {
+
+
+    // Use this for initialization
+    void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+ 
+	}
+}
