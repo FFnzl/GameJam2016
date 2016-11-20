034765edbe693a8ee894760133c0892dbed67a36
Daniel Henseler <danielhenseler@badcodes.de>
2016-11-20 17:20:22 +0100

    Added Main Music Manager

---
+++ Assets/dan/scripts/Sparkling.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sparkling : MonoBehaviour {
+	private ParticleSystem[] _particles;
+
+	private void Start () {
+
+	}
+
+	public void PlaySparkle (Vector3 pWorldPos) {
+		transform.position = pWorldPos;
+
+		foreach (ParticleSystem ps in _particles) {
+
+		}
+	}
+}
