58ed956f82c99dce3166bd4de0ce5b2097c63a88
Florian Fenzl <florian.fenzl@mni.thm.de>
2016-11-20 13:44:41 +0100

    Made Minimap. Changed some prefabs

---
+++ Assets/florian/scripts/FollowController.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowController : MonoBehaviour {
+
+    public Transform follow;
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = new Vector3(follow.position.x, follow.position.y, transform.position.z);
+    }
+}
