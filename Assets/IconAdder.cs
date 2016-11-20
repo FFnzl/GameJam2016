be6ff8fab697ee504986a82609825b24145225b6
Florian Fenzl <florian.fenzl@mni.thm.de>
2016-11-20 15:18:49 +0100

    Changed some prefabs

---
+++ Assets/IconAdder.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IconAdder : MonoBehaviour {
+
+    public GameObject IconPrefab;
+
+	// Use this for initialization
+	void Start () {
+       GameObject o = Instantiate(IconPrefab, transform.position, Quaternion.identity) as GameObject;
+        //o.transform.parent = transform;
+
+	}
+
+}
