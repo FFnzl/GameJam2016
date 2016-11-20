68c9efb994fba4775c109c7eaaae6e520470137d
Dominik <fahldomi@hotmail.de>
2016-11-20 18:22:34 +0100

    Menus and stuff

---
+++ Assets/dom/prefabs/Credits/backButtonBehaviour.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using DG.Tweening;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class backButtonBehaviour : MonoBehaviour {
+    private Color[] colors = { new Color(0.7f, 0, 0.7f), new Color(0, 0, 0.7f),
+                               new Color(0, 0.7f, 0.7f), new Color(0, 0.7f, 0),
+                               new Color(0.7f, 0.7f, 0), new Color(0.7f, 0, 0)};
+    private Image img;
+    [SerializeField]
+    private float duration;
+    private int colorInc;
+    private bool tweening = false;
+
+    // Use this for initialization
+    void Start()
+    {
+        img = GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!tweening) changeColor();
+    }
+
+    private void changeColor()
+    {
+        tweening = true;
+        DOTween.To(() => img.color, x => img.color = x, colors[colorInc], duration).OnComplete(() => tweening = false);
+        colorInc = ++colorInc % colors.Length;
+    }
+
+    public void goBack()
+    {
+        SceneManager.LoadScene("mainScene");
+    }
+}
---
+++ Assets/dom/prefabs/Menu/buttonBehaviour.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class buttonBehaviour : MonoBehaviour {
+
+    public void quitGame()
+    {
+        Application.Quit();
+    }
+
+    public void enterCredits()
+    {
+        SceneManager.LoadScene("mainScene");
+    }
+
+    public void startGame()
+    {
+        SceneManager.LoadScene("mainScene");
+    }
+}
