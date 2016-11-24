972029a017525faced623d07e006d5da17be360f
Daniel Henseler <danielhenseler@badcodes.de>
2016-11-24 18:30:49 +0100

    Renamed many files. Also added room nested prefabs support. Yay!

---
+++ Assets/scripts/FurnitureHelper.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+public class FurnitureHelper : MonoBehaviour {
+	[Serializable]
+	public struct FurnitureData {
+		public FurnitureData (UnityEngine.Object pPrefab, Vector3 pPos, Quaternion pRot) { Prefab = pPrefab; Position = pPos; Rotation = pRot; }
+		public UnityEngine.Object Prefab;
+		public Vector3 Position;
+		public Quaternion Rotation;
+	}
+
+	[SerializeField]
+	private GameObject _prefabsContainer;
+	public GameObject PrefabsContainer { get {return _prefabsContainer; } }
+
+	[SerializeField]
+	private FurnitureData[] _furniturePrefabs = new FurnitureData[0];
+	public FurnitureData[] FurniturePrefabs { get { return _furniturePrefabs; } }
+
+	public void AddFurniture (FurnitureData pData) {
+		FurnitureData[] temp = _furniturePrefabs;
+		_furniturePrefabs = new FurnitureData[temp.Length + 1];
+
+		for (int i = 0; i < temp.Length; ++i) {
+			_furniturePrefabs[i] = temp[i];
+		}
+
+		_furniturePrefabs[temp.Length] = pData;
+	}
+
+	public void ClearFurniture () {
+		_furniturePrefabs = new FurnitureData[0];
+	}
+
+	private void Awake () {
+		foreach (FurnitureData fd in _furniturePrefabs) {
+			GameObject.Instantiate(fd.Prefab, fd.Position, fd.Rotation, _prefabsContainer.transform);
+		}
+	}
+}
---
+++ Assets/scripts/editor/FurnitureHelper_Inspector.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+using System.Collections.Generic;
+
+[CustomEditor(typeof(FurnitureHelper))]
+public class FurnitureHelper_Inspector : Editor {
+	private FurnitureHelper _helper;
+	private Object _cachedPrefabInstance;
+
+	public void OnEnable () {
+		_helper = (FurnitureHelper)target;
+		_cachedPrefabInstance = PrefabUtility.GetPrefabParent(_helper.gameObject);
+
+		if (_cachedPrefabInstance != null) {
+			PrefabUtility.DisconnectPrefabInstance(_helper.gameObject);
+		}
+	}
+
+	public override void OnInspectorGUI () {
+		DrawDefaultInspector();
+
+		_helper = (FurnitureHelper)target;
+
+		Debug.Log(_helper.FurniturePrefabs.Length);
+
+		foreach (FurnitureHelper.FurnitureData t in _helper.FurniturePrefabs) {
+			GUILayout.Label(t.Prefab.name + " Pos: " + t.Position + " Rot: " + t.Rotation.eulerAngles);
+		}
+
+		if (GUILayout.Button("Save Room")) {
+			if (_cachedPrefabInstance != null) {
+				saveFurniturePrefabs();
+				PrefabUtility.ReplacePrefab(_helper.gameObject, _cachedPrefabInstance, ReplacePrefabOptions.Default);
+				PrefabUtility.DisconnectPrefabInstance(_helper.gameObject);
+				DestroyImmediate(_helper.gameObject);
+			} else {
+				Debug.LogError("You should create a prefab first, otherwise nothin is saved.");
+			}
+		} else if (GUILayout.Button("Load Furniture")) {
+			loadFurniturePrefabs();
+		} else if (GUILayout.Button("Clear Saved Furniture")) {
+			_helper.ClearFurniture();
+		}
+	}
+
+	private void saveFurniturePrefabs () {
+		_helper.ClearFurniture();
+
+		foreach (Transform t in _helper.PrefabsContainer.transform) {
+			if (t.gameObject != _helper.PrefabsContainer && PrefabUtility.GetPrefabParent(t.gameObject) != null) {
+				_helper.AddFurniture(new FurnitureHelper.FurnitureData(PrefabUtility.GetPrefabParent(t.gameObject), t.localPosition, t.localRotation));
+			}
+		}
+
+		clearFurnitureContainer();
+
+		EditorUtility.SetDirty(_helper);
+	}
+
+	private void loadFurniturePrefabs () {
+		clearFurnitureContainer();
+
+		foreach (FurnitureHelper.FurnitureData t in _helper.FurniturePrefabs) {
+			GameObject go = PrefabUtility.InstantiatePrefab(t.Prefab) as GameObject;
+			go.transform.parent = _helper.PrefabsContainer.transform;
+			go.transform.localPosition = t.Position;
+			go.transform.localRotation = t.Rotation;
+		}
+	}
+
+	private void clearFurnitureContainer () {
+		Debug.Log(_helper.PrefabsContainer.transform.childCount);
+		List<GameObject> destroyList = new List<GameObject>();
+
+		foreach (Transform t in _helper.PrefabsContainer.transform) {
+			if (t.gameObject != _helper.PrefabsContainer && PrefabUtility.GetPrefabParent(t.gameObject) != null) {
+				destroyList.Add(t.gameObject);
+			}
+		}
+
+		foreach (GameObject g in destroyList) {
+			DestroyImmediate(g);
+		}
+	}
+}
